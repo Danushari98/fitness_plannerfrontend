cc33173a197c66b5fb44ba6200e97e623b02e600
Your Name <you@example.com>
2026-02-03 11:34:05 +0530

    first commit

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api", // change if your backend uses other port
+  baseURL: "https://fitnessgym-backend.onrender.com/api",
+
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
